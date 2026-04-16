99b58944277f6f3e2baa1a05fc032d7d0b0b75db
usamasaboo <162786136+usamasaboo@users.noreply.github.com>
2026-04-16 21:35:58 +0530

    Update main.py

--- Cancer-Detection-main/main.py
+++ Cancer-Detection-main/main.py
@@ -196,6 +196,12 @@ print(f"\n✅ Model saved at {save_file}")
 
 from flask import Flask, render_template
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 app = Flask(__name__)
 
 @app.route('/')
